1897324521cf247d2eba00a1c2c442922015d879
anvts <worontsov.andrey@gmail.com>
2024-07-08 08:43:29 +0300

    Creates user fixture and test for finding user in repository

--- tests/Functional/Users/Infrastructure/Repository/UserRepositoryTest.php
+++ tests/Functional/Users/Infrastructure/Repository/UserRepositoryTest.php
@@ -2,22 +2,27 @@
 
 namespace App\Tests\Functional\Users\Infrastructure\Repository;
 
+use App\Tests\Resource\Fixture\UserFixture;
 use App\Users\Domain\Factory\UserFactory;
 use App\Users\Infrastructure\Repository\UserRepository;
 use Faker\Factory;
 use Faker\Generator;
+use Liip\TestFixturesBundle\Services\DatabaseToolCollection;
+use Liip\TestFixturesBundle\Services\DatabaseTools\AbstractDatabaseTool;
 use Symfony\Bundle\FrameworkBundle\Test\WebTestCase;
 
 class UserRepositoryTest extends WebTestCase
 {
     private UserRepository $repository;
     private Generator $faker;
+    private AbstractDatabaseTool $databaseTool;
 
     protected function setUp(): void
     {
         parent::setUp();
         $this->repository = static::getContainer()->get(UserRepository::class);
         $this->faker = Factory::create();
+        $this->databaseTool = static::getContainer()->get(DatabaseToolCollection::class)->get();
     }
 
     public function testUserAdd(): void
@@ -32,6 +37,19 @@ class UserRepositoryTest extends WebTestCase
 
         // assert
         $foundUser = $this->repository->findByUlid($user->getUlid());
+        $this->assertEquals($user->getUlid(), $foundUser->getUlid());
+    }
+
+    public function testUserFound(): void
+    {
+        // arrange
+        $executor = $this->databaseTool->loadFixtures([UserFixture::class]);
+        $user = $executor->getReferenceRepository()->getReference(UserFixture::REFERENCE);
+
+        // act
+        $foundUser = $this->repository->findByUlid($user->getUlid());
+
+        // assert
         $this->assertEquals($user->getUlid(), $foundUser->getUlid());
     }
 }
